8065e75486d8a7684eaa86da41fb4a90f491a995
folland87 <frederic@olland.me>
2020-11-26 10:21:41 +0100

    edit version

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import Avatar from './components/Avatar';
 import Button from './components/Button';
 import Box from './components/Box';
 import Spinner from './components/Spinner';
+import Pagination from './components/Pagination';
 import { ThemeContextProvider } from './hooks/useTheme';
-export { Avatar, Button, Box, Spinner, ThemeContextProvider }
+export { Avatar, Button, Box, Pagination, Spinner, ThemeContextProvider };
