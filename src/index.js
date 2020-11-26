c8b5062916954c1e513cc1a253b32c83e33b8883
folland87 <frederic@olland.me>
2020-11-26 09:39:08 +0100

    add spinner

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Avatar from './components/Avatar';
 import Button from './components/Button';
 import Box from './components/Box';
+import Spinner from './components/Spinner';
 import { ThemeContextProvider } from './hooks/useTheme';
-export { Avatar, Button, Box, ThemeContextProvider }
+export { Avatar, Button, Box, Spinner, ThemeContextProvider }
